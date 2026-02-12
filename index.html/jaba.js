1c38bdcc0f420d152e552123ee7395d18b1a37f9
mdsaiket936-cpu <mdsaiket936@gmail.com>
2026-02-12 23:43:46 +0600

    Create jaba.js

---
+++ index.html/jaba.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBp8_CJZq9qX6VnLkqY1X9qY1X9qY1X9qY1",
+  authDomain: "election-results-live.firebaseapp.com",
+  databaseURL: "https://election-results-live-default-rtdb.firebaseio.com",
+  projectId: "election-results-live",
+  storageBucket: "election-results-live.appspot.com",
+  messagingSenderId: "123456789012",
+  appId: "1:123456789012:web:abcdef1234567890"
+};
